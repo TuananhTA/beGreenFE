ab7d1beb04dcfb0a0af80b49153535c1d3c86017
An Tran Thi Thuy <anttt@its.local>
2025-02-21 15:10:25 +0700

    Add 'Promotion Management' tab to sidebar

--- src/app/admin/SideBar.js
+++ src/app/admin/SideBar.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
-import {FaMoneyBillAlt,FaTimes,FaBars,FaChevronDown } from 'react-icons/fa';
+import { FaMoneyBillAlt, FaTimes, FaBars, FaChevronDown } from 'react-icons/fa';
 import { FaChartPie, FaBoxesPacking } from "react-icons/fa6";
-import { MdOutlinePayment } from "react-icons/md";
+import { MdOutlinePayment, MdLocalOffer } from "react-icons/md";
 
 const SideBar = () => {
     const [isOpen, setIsOpen] = useState(true);
@@ -20,9 +20,8 @@ const SideBar = () => {
         <div className="flex">
             {/* Sidebar */}
             <div
-                className={`bg-white shadow-lg h-screen flex flex-col transition-all duration-300 ${
-                    isOpen ? "w-64" : "w-16"
-                }`}
+                className={`bg-white shadow-lg h-screen flex flex-col transition-all duration-300 ${isOpen ? "w-64" : "w-16"
+                    }`}
             >
                 {/* Header Sidebar */}
                 <div className="p-4 flex justify-between items-center">
@@ -37,15 +36,15 @@ const SideBar = () => {
                 {/* Menu */}
                 <nav className="mt-4 flex-grow">
                     <a href="#" className=" mt-2 mb-2 flex items-center text-gray-700 py-2 px-4 hover:bg-gray-200 transition-all">
-                        <FaChartPie  className="text-lg"/>
+                        <FaChartPie className="text-lg" />
                         <span className={`ml-3 text-lg transition-all ${!isOpen && "hidden"}`}>Thống kê</span>
                     </a>
                     <a href="/admin/order" className="  mt-2 mb-2 flex items-center text-gray-700 py-2 px-4 hover:bg-gray-200 transition-all">
-                        <FaMoneyBillAlt className="text-lg"/>
+                        <FaMoneyBillAlt className="text-lg" />
                         <span className={`ml-3 text-lg transition-all ${!isOpen && "hidden"}`}>Bán hàng</span>
                     </a>
                     <a href="#" className=" mt-2 mb-2 flex items-center text-gray-700 py-2 px-4 hover:bg-gray-200 transition-all">
-                        <MdOutlinePayment className="text-lg"/>
+                        <MdOutlinePayment className="text-lg" />
                         <span className={`ml-3 text-lg transition-all ${!isOpen && "hidden"}`}>Quán lý hóa đơn</span>
                     </a>
                     <div className="  mt-2 mb-2 relative">
@@ -53,17 +52,37 @@ const SideBar = () => {
                             className="flex w-full items-center text-gray-700 py-2 px-4 hover:bg-gray-200 transition-all"
                             onClick={() => toggleMenu("thongke")}
                         >
-                            <FaBoxesPacking   className="text-lg"/>
+                            <FaBoxesPacking className="text-lg" />
                             <span className={`ml-3 text-lg text-justify flex-1 transition-all ${!isOpen && "hidden"}`}>
                                 Quản lý sản phẩm
                             </span>
                             {isOpen && <FaChevronDown
-                                className={`text-sm transition-transform ${openMenu === "thongke" ? "rotate-180" : ""}`}/>}
+                                className={`text-sm transition-transform ${openMenu === "thongke" ? "rotate-180" : ""}`} />}
                         </button>
                         <div
                             className={`overflow-hidden transition-all ${openMenu === "thongke" ? "max-h-40" : "max-h-0"}`}>
                             <a href="/admin/product" className="block pl-12 pr-4 py-2 text-gray-600 hover:bg-gray-100">Sản phẩm</a>
                             <a href="#" className="block pl-12 pr-4 py-2 text-gray-600 hover:bg-gray-100">Đơn vị</a>
+                        </div>
+                    </div>
+                    <div className="mt-2 mb-2 relative">
+                        <button
+                            className="flex w-full items-center text-gray-700 py-2 px-4 hover:bg-gray-200 transition-all"
+                            onClick={() => toggleMenu("promotion")}
+                        >
+                            <MdLocalOffer className="text-lg" />
+                            <span className="ml-3 text-lg flex-1 transition-all">Quản lý khuyến mãi</span>
+                            <FaChevronDown
+                                className={`text-sm transition-transform ${openMenu === "promotion" ? "rotate-180" : ""}`}
+                            />
+                        </button>
+                        <div className={`overflow-hidden transition-all ${openMenu === "promotion" ? "max-h-40" : "max-h-0"}`}>
+                            <a href="/admin/voucher" className="block pl-12 pr-4 py-2 text-gray-600 hover:bg-gray-100">
+                                Voucher
+                            </a>
+                            <a href="/admin/promotion" className="block pl-12 pr-4 py-2 text-gray-600 hover:bg-gray-100">
+                                Chương trình khuyến mãi
+                            </a>
                         </div>
                     </div>
                 </nav>
